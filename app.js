3610107ec50165852990b82506ddd60a8f3cf415
dielawn <dielawnmerkill@gmail.com>
2024-03-29 06:09:42 -0600

    express version of the node server built in the node_tutorial

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 8080;
+
+//middleware to serve static files
+app.use(express.static('public'));
+
+//routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+//catch all for errors
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, '404.html'), err => {
+        if (err) {
+            console.log(err)
+            res.status(404).send('404 Not Found')
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+});
